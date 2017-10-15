f2b31785e3fd919564557206271041703de9df3f
malli <mallipachha@gmail.com>
2017-10-15 20:59:16 +0530

    first commit here

---
+++ src/cpp/LoginResponse.java
@@ -0,0 +1,14 @@
+package com.mss.net;
+
+public class LoginResponse extends ServiceResponse{
+	private Long id;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+}
